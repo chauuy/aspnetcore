aa590a0cb19ac70190db9b52ac131eb1ad709357
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-13 12:05:20 +0200

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -13,16 +13,7 @@ namespace PracticalAspNetCore
             {
                 // Duplicate the code below and write more messages. Save and refresh your browser to see the result.
                 //await context.Response.WriteAsync("Hello world. Make sure you run this app using 'dotnet watch run'.");
-                await context.Response.WriteAsync(return new ContentResult
-                 {
-                  Content = @"
-                  <html><body>
-                  <h1>Hello world</h1>
-                  Hello world. 
-                  </body></html> ",
-                ContentType = "text/html"
-                  };              
-                );
+                await context.Response.WriteAsync("<html><body><h1>Hello world</h1>Hello world.</body></html> ");
             });
         }
     }
