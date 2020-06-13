b205e2e47a3409be4ce1101b4c21da750bb76eed
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-13 13:35:43 +0200

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -19,7 +19,7 @@ namespace PracticalAspNetCore
     var proc = Process.GetCurrentProcess();
     var page = "<html><body><h1>HELLO-WORLD SAMPLE .NET Core</h1><h5>Environment</h5><p>@"+RuntimeInformation.FrameworkDescription+"</p><p>"+RuntimeInformation.OSDescription+"</p>";
 page=page+"<h5>Metrics</h5><table width=500 align=center><tr><td>Containerized</td><td>"+Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") is object ? "true" : "false")+"</td></tr><tr><td>CPU cores</td>";
-page=page+"<td>"+Environment.ProcessorCount+"</td></tr><tr><td>Memory, current usage (bytes)</td><td>"+process.WorkingSet64+"</td></tr><tr><td>Memory, max available (bytes)</td><td>"+process.MaxWorkingSet+"</td>";
+page=page+"<td>"+Environment.ProcessorCount+"</td></tr><tr><td>Memory, current usage (bytes)</td><td>"+proc.WorkingSet64+"</td></tr><tr><td>Memory, max available (bytes)</td><td>"+proc.MaxWorkingSet+"</td>";
 page=page+"</body></html>";
                 
                 // Duplicate the code below and write more messages. Save and refresh your browser to see the result.
