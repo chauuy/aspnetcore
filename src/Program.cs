c7de5853d4701d2a569186f9654495419ab30da7
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-13 13:40:17 +0200

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using System.IO;
 using System.Diagnostics;
-using static System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 
 namespace PracticalAspNetCore
 {
